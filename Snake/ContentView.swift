5e74df92fb15867f5502a35813656292068972c4
Astemir <astemireleev@gmail.com>
2024-08-22 07:52:13 +0300

    Initial Commit

---
+++ Snake/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snake
+//
+//  Created by Astemir Eleev on 22/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snake/SnakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeApp.swift
+//  Snake
+//
+//  Created by Astemir Eleev on 22/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SnakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
